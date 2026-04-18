01760ee5a2a3b9e559c14396c943bede85cfd9f7
roshini-098 <roshiniganji123@gmail.com>
2026-04-18 20:48:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+.container {
+  width: 700px;
+  margin: auto;
+  text-align: center;
+  font-family: Arial;
+}
+
+.form {
+  margin-bottom: 20px;
+}
+
+input {
+  margin: 5px;
+  padding: 8px;
+}
+
+button {
+  padding: 8px 15px;
+  background-color: green;
+  color: white;
+  border: none;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ccc;
+  padding: 8px;
+}
+
+.delete {
+  background-color: red;
+}
